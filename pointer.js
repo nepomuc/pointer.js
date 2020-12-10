29490b81ac63209415252fb05de7ace09ec34ed6
Nepomuk Leutschacher <864660+nepomuc@users.noreply.github.com>
2020-12-10 19:17:38 +0100

    Update pointer.js
    Optimized a bunch of things and added a lot more parameters for setting up pointer.js

--- pointer.js
+++ pointer.js
@@ -1,76 +1,124 @@
 /* 
-    pointer.js was created by OwL for use on websites, 
-     and can be found at https://seattleowl.com/pointer.
+    pointer.js was created by OwL and Nepomuc for use on websites.
 */
 
-const pointer = document.createElement("div")
-pointer.id = "pointer-dot"
-const ring = document.createElement("div")
-ring.id = "pointer-ring"
-document.body.insertBefore(pointer, document.body.children[0])
-document.body.insertBefore(ring, document.body.children[0])
-
-let mouseX = -100
-let mouseY = -100
-let ringX = -100
-let ringY = -100
-let isHover = false
-let mouseDown = false
 const init_pointer = (options) => {
-
-    window.onmousemove = (mouse) => {
-        mouseX = mouse.clientX
-        mouseY = mouse.clientY
-    }
-
-    window.onmousedown = (mouse) => {
-        mouseDown = true
-    }
-
-    window.onmouseup = (mouse) => {
-        mouseDown = false
-    }
-
-    const trace = (a, b, n) => {
-        return (1 - n) * a + n * b;
-    }
-    window["trace"] = trace
-
-    const getOption = (option) => {
-        let defaultObj = {
-            pointerColor: "#750c7e",
-            ringSize: 15,
-            ringClickSize: (options["ringSize"] || 15) - 5,
-        }
-        if (options[option] == undefined) {
-            return defaultObj[option]
-        } else {
-            return options[option]
+    
+    const isTouchDevice = () => {
+        try {
+            document.createEvent("TouchEvent");
+            return true;
+        } catch (e) {
+            return false;
         }
     }
+    
+    if (!isTouchDevice()) {
+        
+        let mouseX = -100
+        let mouseY = -100
+        let ringX = -100
+        let ringY = -100
+        let isHover = false
+        let mouseDown = false
+        let mouseNeverMovedYet = true
+        
+        const pointer = document.createElement("div")
+        pointer.id = "pointer-dot"
+        const ring = document.createElement("div")
+        ring.id = "pointer-ring"
 
-    const render = () => {
-        ringX = trace(ringX, mouseX, 0.2)
-        ringY = trace(ringY, mouseY, 0.2)
-
-        if (document.querySelector(".p-action-click:hover")) {
-            pointer.style.borderColor = getOption("pointerColor")
-            isHover = true
-        } else {
-            pointer.style.borderColor = "white"
-            isHover = false
+        const trace = (a, b, n) => {
+            return (1 - n) * a + n * b;
         }
-        ring.style.borderColor = getOption("pointerColor")
-        if (mouseDown) {
-            ring.style.padding = getOption("ringClickSize") + "px"
-        } else {
-            ring.style.padding = getOption("ringSize") + "px"
+        window["trace"] = trace
+
+        const getOption = (option) => {
+            let defaultObj = {
+                pointerFollowStiffness: 0.2,
+                pointerSize: 2.5,
+                pointerColor: "grey",
+                pointerBoxShadow: "0px 0px 0px 1px #FFFFFF",
+                ringOutlineColor: "grey",
+                ringInsideSize: 15,
+                ringInsideColor: "tansparent",
+                ringBoxShadow: "0px 0px 0px 1px #FFFFFF",
+                ringOutlineSize: 2,
+                ringOutlineStyle: "solid",
+                ringInsideSizeClick: (options["ringInsideSize"] || 15) - 5,
+            }
+            if (options[option] == undefined) {
+                return defaultObj[option]
+            } else {
+                return options[option]
+            }
         }
 
-        pointer.style.transform = `translate(${mouseX}px, ${mouseY}px)`
-        ring.style.transform = `translate(${ringX - (mouseDown ? getOption("ringClickSize") : getOption("ringSize"))}px, ${ringY - (mouseDown ? getOption("ringClickSize") : getOption("ringSize"))}px)`
+        const getRingTransformOffset = () => {
+            return (mouseDown ? getOption("ringInsideSizeClick")/2+getOption("ringOutlineSize") : getOption("ringInsideSize")/2+getOption("ringOutlineSize"))
+        }
 
-        requestAnimationFrame(render)
+        const ringUpdatePosition = (x,y) => {
+            ring.style.transform = `translate(${x - getRingTransformOffset()}px, ${y - getRingTransformOffset()}px)`
+        }
+
+        const pointerUpdatePosition = (x,y) => {
+            pointer.style.transform = `translate(${x-getOption("pointerSize")/2}px, ${y-getOption("pointerSize")/2}px)`
+        }
+
+        window.onmousemove = (mouse) => {
+            mouseX = mouse.clientX
+            mouseY = mouse.clientY
+            if(mouseNeverMovedYet) {
+                mouseNeverMovedYet = false
+                pointerUpdatePosition(mouseX,mouseY)
+                ringX = mouseX
+                ringY = mouseY
+                ringUpdatePosition(ringX,ringY)
+                requestAnimationFrame(render)
+            }
+        }
+
+        window.onmousedown = (mouse) => {
+            mouseDown = true
+        }
+
+        window.onmouseup = (mouse) => {
+            mouseDown = false
+        }
+
+        const render = () => {
+            ringX = trace(ringX, mouseX, getOption("pointerFollowStiffness"))
+            ringY = trace(ringY, mouseY, getOption("pointerFollowStiffness"))
+
+            if (mouseDown) {
+                ring.style.padding = getOption("ringInsideSizeClick")/2 + "px"
+                ring.style.borderRadius = getOption("ringInsideSizeClick")+getOption("ringOutlineSize") + "px"
+            } else {
+                ring.style.padding = getOption("ringInsideSize")/2 + "px"
+                ring.style.borderRadius = getOption("ringInsideSize")+getOption("ringOutlineSize") + "px"
+            }            
+
+            pointerUpdatePosition(mouseX,mouseY)
+            ringUpdatePosition(ringX,ringY)
+
+            requestAnimationFrame(render)
+        }
+
+        document.body.style.cursor = "none"
+        document.body.insertBefore(pointer, document.body.children[0])
+        document.body.insertBefore(ring, document.body.children[0])
+
+        pointer.style.padding = getOption("pointerSize")/2 + "px"
+        pointer.style.borderRadius = getOption("pointerSize") + "px"
+        pointer.style.borderColor = getOption("pointerColor")
+        pointer.style.backgroundColor = getOption("pointerColor")
+        pointer.style.boxShadow = getOption('pointerBoxShadow')
+        
+        ring.style.backgroundColor = getOption("ringInsideColor")
+        ring.style.borderWidth = getOption("ringOutlineSize") + "px"
+        ring.style.borderColor = getOption("ringOutlineColor")
+        ring.style.borderStyle = getOption("ringOutlineStyle")
+        ring.style.boxShadow = getOption('ringBoxShadow')
     }
-    requestAnimationFrame(render)
 }
